a8cf313b060ee481c67acc4506c032a63d6784f4
alyssadev <alyssa.dev.smith@gmail.com>
2020-11-20 19:42:55 +0000

    Support new reply feature in Rapptz/discord.py#6061

--- emojinator.py
+++ emojinator.py
@@ -62,7 +62,7 @@ async def emoji(message):
         try:
             await message.author.send(__emoji_help)
         except discord.Forbidden:
-            await message.channel.send(__emoji_help)
+            await message.reply(__emoji_help)
         return
     if cmd == "import":
         out = []
@@ -71,24 +71,24 @@ async def emoji(message):
             success, resp = await get_bytes_from_url(f"https://cdn.discordapp.com/emojis/{emoji_id}." + ("gif" if animated else "png"))
             if not success:
                 print(repr(resp))
-                await message.channel.send(resp)
+                await message.reply(resp)
                 continue
             try:
                 emoji = await message.guild.create_custom_emoji(name=shortname, image=resp, reason=f"Added by {message.author.name}#{message.author.discriminator}")
             except discord.errors.HTTPException as e:
                 err = "!emoji failed on {}: {}".format(emoji_inp[1:-1], e.text.split("\n")[1])
-                await message.channel.send(err)
+                await message.reply(err)
                 print(err)
                 continue
             except discord.errors.InvalidArgument as e:
                 err = "!emoji failed on {}: Invalid argument: {}".format(emoji_inp[1:-1], e.text.replace("\n", ""))
-                await message.channel.send(err)
+                await message.reply(err)
                 print(err)
                 continue
             out.append(str(emoji))
         response = get_emoji_count(message.guild) + "\n" + " ".join(out)
         print(repr(response))
-        await message.channel.send(response)
+        await message.reply(response)
     elif cmd == "image":
         out = []
         for arg,attachment in zip(args, message.attachments):
@@ -96,34 +96,34 @@ async def emoji(message):
             try:
                 emoji = await message.guild.create_custom_emoji(name=arg, image=img_bytes, reason=f"Added by {message.author.name}#{message.author.discriminator}")
             except discord.errors.HTTPException as e:
-                await message.channel.send("!emoji failed on {}: {}".format(arg, e.text.split("\n")[1]))
+                await message.reply("!emoji failed on {}: {}".format(arg, e.text.split("\n")[1]))
                 continue
             out.append(str(emoji))
-        await message.channel.send(get_emoji_count(message.guild) + "\n" + " ".join(out))
+        await message.reply(get_emoji_count(message.guild) + "\n" + " ".join(out))
     if cmd == "url":
         name, url = args
         success, resp = await get_bytes_from_url(url)
         if not success:
-            await message.channel.send(resp)
+            await message.reply(resp)
             return
         try:
             emoji = await message.guild.create_custom_emoji(name=name, image=resp, reason=f"Added by {message.author.name}#{message.author.discriminator}")
         except discord.errors.HTTPException as e:
-            await message.channel.send("!emoji failed: {}".format(e.text))
+            await message.reply("!emoji failed: {}".format(e.text))
             return
-        await message.channel.send(get_emoji_count(message.guild) + "\n" + str(emoji))
+        await message.reply(get_emoji_count(message.guild) + "\n" + str(emoji))
     elif cmd == "geturl":
         out = []
         for emoji_inp in args:
             animated,shortname,emoji_id = emoji_inp[1:-1].split(":")
             out.append(f"<https://cdn.discordapp.com/emojis/{emoji_id}." + ("gif" if animated else "png") + ">")
-        await message.channel.send("\n".join(out))
+        await message.reply("\n".join(out))
     elif cmd == "mashup":
         with open("emojimashup.json") as f:
             mashup_data = load(f)
         mashup = choice(mashup_data)
         await asyncio.sleep(1)
-        await message.channel.send("{1} = https://pbs.twimg.com/media/{0}.jpg".format(*mashup))
+        await message.reply("{1} = https://pbs.twimg.com/media/{0}.jpg".format(*mashup))
 
 
 @client.event
@@ -142,10 +142,10 @@ async def on_message(message):
             async with message.channel.typing():
                 await emoji(message)
         else:
-            await message.channel.send("No 'Manage Emoji' permission")
+            await message.reply("No 'Manage Emoji' permission")
 
     if message.content.lower() == "!invite":
-        await message.channel.send(discord.utils.oauth_url(client.user.id, permissions=discord.Permissions(1073743872)))
+        await message.reply(discord.utils.oauth_url(client.user.id, permissions=discord.Permissions(1073743872)))
 
 @client.event
 async def on_ready():
